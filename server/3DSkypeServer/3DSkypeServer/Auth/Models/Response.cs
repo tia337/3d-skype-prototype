1740c6d4c57c0fa9d135d3b73381eda7d123e118
Yaroslav Zghurovskyi <yzghurovskyi@foundationmedicine.com>
2020-11-10 16:36:57 +0200

    backend added auth and deployment

---
+++ server/3DSkypeServer/3DSkypeServer/Auth/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace _3DSkypeServer.Auth.Models
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ server/3DSkypeServer/3DSkypeServer/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using _3DSkypeServer.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace _3DSkypeServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        [HttpGet, Route("user")]
+        public string GetUser() => "Successful response for Authorized user";
+
+        [Authorize(Roles=UserRoles.Admin)]
+        [HttpGet, Route("admin")]
+        public string GetAdminUser() => "Successful response for Authorized Admin user";
+    }
+}
