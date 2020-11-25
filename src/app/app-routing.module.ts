8631d031d859d178037bee5b77d98924a443ab9d
Illia Telefus <i.telefus@nodeart.io>
2020-11-25 15:16:15 +0200

    added video component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { AuthGuard } from './_helpers';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const profileModule = () => import('./profile/profile.module').then(x => x.ProfileModule);
+const videoCallModule = () => import('./video-call/video-call.module').then(x => x.VideoCallModule);
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'account', loadChildren: accountModule },
     { path: 'profile', loadChildren: profileModule, canActivate: [AuthGuard] },
+    { path: 'video', loadChildren: videoCallModule },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
---
+++ src/app/video-call/video-call.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-call',
+  templateUrl: './video-call.component.html',
+  styleUrls: ['./video-call.component.less']
+})
+export class VideoCallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    
+  }
+
+}
